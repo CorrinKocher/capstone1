c28d377c14b58b77c6308c9a65e747725e502ea6
Corrin <corrin.kocher1@gmail.com>
2020-10-10 11:06:18 -0400

    pushing bad unit tests

--- CapstoneTests/UnitTest1.cs
+++ CapstoneTests/UnitTest1.cs
@@ -1,4 +1,8 @@
+using Capstone.Classes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace CapstoneTests
 {
@@ -6,8 +10,38 @@ namespace CapstoneTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        
+        [DataRow(5, 5)]
+        //[DataRow(0, 0)]
+        //[DataRow(-1, 0)]
+        //[DataRow(10, 10)]
+        //[DataRow(5001, 0)]
+
+        public void TestCateringMethods(int MoneyAdded, decimal ExpectedBalance)
         {
+            Catering catering = new Catering();
+            catering.AddMoney((decimal)MoneyAdded);
+            Assert.IsTrue(ExpectedBalance == (decimal)MoneyAdded);
+            //Assert.AreEqual(ExpectedBalance, MoneyAdded);
+            //should I change test method to return a decimal instead of a void?
+            //do i need to cast ExpectedBalance into a decimal as well?
+        
         }
+
+        [TestMethod]
+        [DataRow(5, 5)]
+        
+        public decimal TestAddMoneyMethod( decimal MoneyAdded, decimal ExpectedBalance)
+        {
+            Catering catering = new Catering();
+            catering.AddMoney(MoneyAdded);
+            decimal expected = catering.customerMoney;
+            //Assert.AreEqual(ExpectedBalance, catering.customerMoney);
+            return catering.customerMoney;
+        }
+
+
+
+
     }
 }
